6a167e3c309725fdb8d8ce7a0b230b6a3adcaecd
Luis Augusto Peña Pereira <lpenap@gmail.com>
2017-05-01 22:09:51 -0400

    Update README documentation

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # oc-energy-monitor
-OpenComputers Lua program to monitor Draconic Energy Core.
-This is a work in progress, there is no final release yet.
+OpenComputers Lua program to monitor Draconic Energy Core. Comes with a text and
+a graphic version.
+
+## Requirements
+- Place an Adapter Block in contact with one of your Energy Pylons
+- Connect the Adapter Block through cables to the computer
+- Only tested with tier 3 computers and screens (3x3 recommended)
+
+## Install
+oppm install energy-monitor
+
+## Executable
+energy-monitor
 
 ## Exit codes
 - 1: Address for energy core not found, check your network cables and check that the EnergyPylon is connected to the computer through Adapter/Cables
--- src/energy-monitor/energy-monitor-graphic.lua
+++ src/energy-monitor/energy-monitor-graphic.lua
@@ -1,1 +1,94 @@
-print ("not implemented yet")
+--------------------------------------------------------------------------------
+-- energy-monitor v0.1 by luisau
+-- OpenComputers program to monitor a single Draconic Evolution Energy Core.
+--
+-- Requirements:
+--    - Place an Adapter Block in contact with one of your Energy Pylons
+--    - Connect the Adapter Block through cables to the computer
+--    - Only tested with tier 3 computers.
+-- Install:
+--    oppm install energy-monitor
+-- Feedback:
+--    Contact me through Discord app: luisau#0826
+-- Repository:
+--    https://github.com/lpenap/oc-energy-monitor
+--------------------------------------------------------------------------------
+
+require("energy-monitor-lib")
+
+--------------------------------------------------------------------------------
+-- Configuration
+--------------------------------------------------------------------------------
+
+-- step: run interval in seconds.
+local step = 1
+
+-- debug: print aditional debug info (usually at start).
+local debug = true
+
+--------------------------------------------------------------------------------
+-- Constants
+--------------------------------------------------------------------------------
+local storageName = "draconic_rf_storage"
+local initDelay = 10
+
+--------------------------------------------------------------------------------
+-- Prints the current energy values
+--------------------------------------------------------------------------------
+function printEnergy (core, term)
+  term.setCursor(2, 1)
+  term.clearLine()
+  term.setCursor(2, 1)
+  term.write("Energy Core (RF): " ..
+    formatNumber(core:getLastEnergyStored()) .. " / " ..
+    formatNumber(core:getMaxEnergyStored()) .. "   (" ..
+    string.format("%.2f", core:getLastPercentStored()) .. "%)")
+end
+
+--------------------------------------------------------------------------------
+-- Prints the change on energy levels
+--------------------------------------------------------------------------------
+function printEnergyChange (change, term)
+  term.setCursor(2, 2)
+  term.clearLine()
+  term.setCursor(2, 2)
+  local gpu = term.gpu()
+  if change >= 0 then
+    gpu.setForeground(0x00FF00)
+  else
+    gpu.setForeground(0xFF00FF)
+  end
+  term.write("    Energy Flow : " .. formatNumber(change) .." RF/t")
+  gpu.setForeground(0xFFFFFF)
+end
+
+--------------------------------------------------------------------------------
+-- Updates values on screen continuously (until interrupted)
+--------------------------------------------------------------------------------
+function run ()
+  local core, term, component =
+    init (storageName, "graphic", debug, initDelay)
+
+  if core == nil then
+    return 1
+  end
+
+  term.clear()
+  os.sleep (step)
+  while true do
+    local energyChange = core:getEnergyChange()
+    printEnergy (core, term)
+    printEnergyChange (energyChange, term)
+    os.sleep(step)
+  end
+end
+
+
+--------------------------------------------------------------------------------
+-- Main Program
+--------------------------------------------------------------------------------
+local exitCode = run ()
+if exitCode ~= 0 then
+  print ("An internal error occurred. Exit code ".. exitCode)
+end
+
